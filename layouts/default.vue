5ffe6382f6f881516d1b87fbc617c2eefda00f60
IlanAzoulay <ilan.azoulay@protonmail.com>
2021-12-16 19:11:26 +0100

    fixed dist problem

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,38 +1,55 @@
 <template>
-    <div :class="`grid grid-cols-${nombre_col}`">
+    <!-- <div :class="`grid grid-cols-${nombre_col} grid-rows-1`"> -->
+    <div class="grid_all">
 
         <!-- Barre a gauche -->
-        <div class="col-span-1">
+        <!-- <div class="col-span-1">
             <!-- <Navbar @scrollTo="goto" :class="`fixed w-1/${nombre_col}`" /> -->
-            <Navbar :class="`fixedBar w-1/${nombre_col}`"/>
+            <!-- <Navbar :class="`fixedBar w-1/${nombre_col}`"/> -->
+        <!-- </div> --> -->
+        <div class="gauche">
+            <Navbar class="fixedBar"/>
         </div>
 
         <!-- Le reste a droite -->
-        <Nuxt :class="`col-span-${(nombre_col-1)}`" />
-
+        <!-- <Nuxt :class="`col-span-${(nombre_col-1)}`" /> -->
+        <Nuxt class="droite"/>
         
     </div>
 </template>
 
+
+<script>
+export default {
+    beforeMount() {
+        let root = document.documentElement;
+        root.style.setProperty('--col-number', 6);
+    }
+}
+</script>
+
 <style scoped>
+    .grid_all {
+        @apply bg-gray_moi;
+        display: grid;
+        grid-template-columns: repeat(var(--col-number), minmax(0, 1fr));
+        grid-template-rows: repeat(1, minmax(0, 1fr));
+    }
     .fixedBar {
         position: fixed;
         left: 0;
         top: 0;
+        width: calc((100/var(--col-number)) * 1%);
     }
-</style>
+    .gauche {
+        grid-column-start: 1;
+        grid-column-end: 2;
+        grid-row-start: 1;
+    }
+    .droite {
+        grid-column-start: 2;
+        grid-column-end: calc(1 + var(--col-number));
+        grid-row-start: 1;
+    }
 
-<script>
-export default {
-    data() {
-        return {
-            nombre_col: 6
-        };
-    },
-    methods: {
-        // goto(refName) {
-        //     this.$store.commit('scrollTo', refName)
-        // }
-    }
-}
-</script>
+</style>
