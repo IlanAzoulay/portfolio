5c663d5d7fd1ae7ea6121f9b357de8c9399d0f88
IlanAzoulay <ilan.azoulay@protonmail.com>
2021-12-17 23:34:16 +0100

    sphere placement and Phi rotation

--- components/SphereItem.vue
+++ components/SphereItem.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="absolute top-0 left-0 w-full h-full">
-        <h1 :style="`left: ${get_pos_x()}vw; top: ${get_pos_y()}vw; filter: ${get_blur()}; font-size: ${get_size()}vw;
+        <h1 :style="`top: ${get_top()}vw; left: ${get_left()}vw; filter: ${get_blur()}; font-size: ${get_size()}vw;
         opacity: ${get_opacity()}`" >
             {{text}}
         </h1>
@@ -13,8 +13,8 @@
 export default {
     props: {
         text: String,
-        input_x: Number,
-        input_z: Number,
+        input_theta: Number,
+        input_phi: Number,
         index: Number,
         number_items: Number,
         radius: Number
@@ -24,77 +24,103 @@ export default {
             pos_x: 0,
             pos_y: 0,
             pos_z: 0,
-            opacity: 100,
-            angle: 0,
-            angle_z: 0,
-            blur: 0,
+            offset_theta: 0,
+            offset_phi: 0,
+            angle_theta: 0,
+            angle_phi: 0,
             blur_max: 0.1,
             size_min: 1,
-            size_max: 2,
-            size: 2,
+            size_max: 2.5,
             opacity_min: 0.5
         };
     },
 
     mounted() {
         this.set_starting_angle();
+        this.update_pos();
+    },
+
+    // Activates when prop changes
+    watch: {
+        "input_theta": function(val, oldVal) {
+            this.update_theta();
+            this.update_pos();
+            console.log("Index: ", this.index, "\t Phi:", this.to_degrees(this.angle_phi),"\t theta: ", this.to_degrees(this.angle_theta));
+        },
+        "input_phi": function(val, oldVal) {
+            this.update_phi();
+            this.update_pos();
+        }
     },
 
     methods: {
         to_radians(degrees){
-            return degrees * (Math.PI/180);
+            return degrees * (Math.PI/180.0);
         },
-        set_starting_angle(){
-            this.angle = this.index * (360/this.number_items)
+        to_degrees(radians){
+            return radians * (180.0/Math.PI);
         },
-        get_pos_x(){
-            // Alterner entre -R et R
-            this.pos_x = this.radius * Math.cos(this.to_radians(this.angle + this.input_x));
 
-            // Alterner entre 0 et 2R
-            return (this.radius + this.pos_x);
+        update_theta(){
+            // Theta entre 0 et 180
+            this.angle_theta = this.to_radians(this.offset_theta + this.input_theta);            
+        },
+        update_phi(){
+            // Phi entre 0 et 360
+            this.angle_phi = this.to_radians(this.offset_phi + this.input_phi);
+        },
+        update_pos(){
+            this.pos_x = this.get_pos_x();
+            this.pos_y = this.get_pos_y();
+            this.pos_z = this.get_pos_z();
+        },
+
+        set_starting_angle(){
+
+            // EXTREME LEARNING
+            var goldenRatio = (1 + Math.sqrt(5))/2;
+            this.angle_phi = this.to_degrees( 2 * Math.PI * this.index / goldenRatio );
+            this.angle_theta = this.to_degrees( Math.acos(1 - 2*(this.index+0.5)/ this.number_items));
+            // x, y, z = cos(phi) * sin(theta), sin(theta) * sin(phi), cos(theta);
+
+            this.offset_theta = this.angle_theta;
+            this.offset_phi = this.angle_phi;
+
+            this.angle_theta = this.to_radians(this.angle_theta);
+            this.angle_phi = this.to_radians(this.angle_phi);
+        },
+
+        get_top(){
+            return this.radius + this.pos_z;
+        },
+        get_left(){
+            return this.radius + this.pos_y;
+        },
+
+        get_pos_x(){
+            return this.radius * Math.cos(this.angle_phi) * Math.sin(this.angle_theta);  // Wiki
         },
         get_pos_y(){
-            this.pos_y = this.radius * Math.sin(this.to_radians(this.angle + this.input_x));
-
-            // Alterner entre 0 et 2R
-            return (this.radius + this.pos_y);
-
-            // console.log("pos_y: " + (this.radius + (Math.sqrt(Math.pow(this.radius, 2) - Math.pow(this.pos_x, 2)))));
-            // return (this.radius + (Math.sqrt(Math.pow(this.radius, 2) - Math.pow(this.pos_x, 2))));
+            return this.radius * Math.sin(this.angle_phi) * Math.sin(this.angle_theta); // Wiki
         },
         get_pos_z(){
-            this.pos_z = this.radius * Math.cos(this.to_radians(this.angle + this.input_z));
-            return this.pos_z;
+            return this.radius * Math.cos(this.angle_theta);  // Wiki
         },
-        get_linear_value(min, max) {
-            this.get_pos_z();
+
+        get_linear_value(coord, min, max) {
             var b = min + ((max - min)/2);
-            return (this.pos_z * ((max - b) / this.radius) + b);
+            return (coord * ((max - b) / this.radius) + b);
         },
         get_blur(){
-            // blur=0 quand on est pile en face (Z = R)
-            // blur = blur_max quand on est tout au fond (Z = -R)
-            // Fonction y = ax + b
-            // blur = (blur_max / 2) * (1 - Z/R)
-
-            this.get_pos_z();
-            this.blur = (this.blur_max / 2) * (1 - (this.pos_z / this.radius));
-            return ("blur(" + this.blur + "vw)");
+            // Blur_min etant 0, on ne le prend pas en compte
+            var blur = (this.blur_max / 2) * (1 - (this.pos_x / this.radius));
+            return ("blur(" + blur + "vw)");
         },
         get_size(){
-            // size = min quand on est au fond (Z = -R)
-            // size = max quand on est devant (Z = R)
-            // fonction y = ax + b
-            // avec a = (max-b)/R et b = min + (max-min)/2
-
-            // var b = this.size_min + (this.size_max - this.size_min)/2;
-            // this.size = this.pos_z * ((this.size_max - b) / this.radius) + b;
-            // return this.size;
-            return this.get_linear_value(this.size_min, this.size_max);
+            return this.get_linear_value(this.pos_x, this.size_min, this.size_max);
         },
         get_opacity(){
-            return this.get_linear_value(this.opacity_min, 1);
+            return this.get_linear_value(this.pos_x, this.opacity_min, 1);
         }
     }
 }
@@ -108,5 +134,6 @@ export default {
         /* font-size: 2vw; */
         position: absolute;
         /* filter: blur(0vw); */
+        transition: all 0.0s;
     }
 </style>
--- components/SphereTest.vue
+++ components/SphereTest.vue
@@ -10,6 +10,9 @@
         <div class="bouton" id="bouton_r" @click="move_front">
             FRONT
         </div>
+        <div class="bouton" id="bouton_r" @click="move_back">
+            BACK
+        </div>
         <!-- Le positionnement RELATIVE permet aux objets enfant de se positionner de maniere relative -->
         <!-- <div class="relative p-16" id="sphere"> -->
         <div class="relative w-screen h-screen bg-gray_moi-light" id="sphere">
@@ -17,11 +20,11 @@
                 class="absolute top-0 left-0"
                 v-for='(item, index) in list' :key='index'
                 :text="item"
-                :input_x="move_x"
-                :input_z="move_z"
+                :input_theta="move_theta"
+                :input_phi="move_phi"
                 :index="index"
                 :number_items="list.length"
-                :radius="10"
+                :radius="radius"
                  />
             <!-- <span class="absolute dot"></span> -->
         </div>
@@ -38,26 +41,37 @@ export default {
     },
     data() {
         return {
-            radius: 10,
-            move_x: 0,
-            move_z: 0,
-            size: {
-                'x': 14,
-                'y': 14
-            },
-            list: ['10', '20', '30', '40', '50', '60']
+            radius: 8,
+            move_theta: 0,
+            move_phi: 0,
+            list: ['10', '20', '30', '40', '50', '60', '70', '80', '90']
         };
     },
 
     methods:{
         move_left(){
-            this.move_x -= 10;
+            this.move_theta -= 10;
+            if (this.move_theta < -360){
+                this.move_theta += 360;
+            }
         },
         move_right(){
-            this.move_x += 10;
+            this.move_theta += 10;
+            if (this.move_theta > 360){
+                this.move_theta -= 360;
+            }
         },
         move_front(){
-            this.move_z += 10;
+            this.move_phi += 10;
+            if (this.move_phi  > 360){
+                this.move_phi -= 360;
+            }
+        },
+        move_back(){
+            this.move_phi -= 10;
+            if (this.move_phi  < -360){
+                this.move_phi += 360;
+            }
         }
     },
 
